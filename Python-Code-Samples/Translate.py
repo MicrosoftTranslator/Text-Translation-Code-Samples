93f00a964e08770b4237a40e1ea1d76bff005159
HamshavathiM <101436046+HamshavathiM@users.noreply.github.com>
2022-03-15 15:23:14 -0700

    Create Translate.py

---
+++ Python-Code-Samples/Translate.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# This simple app uses the '/translate' resource to translate text from
+# one language to another.
+
+# This sample runs on Python 2.7.x and Python 3.x.
+# You may need to install requests and uuid.
+# Run: pip install requests uuid
+
+import os, requests, uuid, json
+
+key_var_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
+if not key_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
+subscription_key = os.environ[key_var_name]
+
+region_var_name = 'TRANSLATOR_TEXT_REGION'
+if not region_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(region_var_name))
+region = os.environ[region_var_name]
+
+endpoint_var_name = 'TRANSLATOR_TEXT_ENDPOINT'
+if not endpoint_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(endpoint_var_name))
+endpoint = os.environ[endpoint_var_name]
+
+# If you encounter any issues with the base_url or path, make sure
+# that you are using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
+path = '/translate?api-version=3.0'
+params = '&from=en&to=de&to=it'
+constructed_url = endpoint + path + params
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Ocp-Apim-Subscription-Region': region,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+body = [{
+    'text' : 'Hello World!'
+}]
+request = requests.post(constructed_url, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ': ')))
