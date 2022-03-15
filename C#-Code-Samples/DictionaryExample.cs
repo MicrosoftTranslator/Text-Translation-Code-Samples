dbf9776096ea312d8f3205b4ab8df1cd5aa0d5fa
HamshavathiM <101436046+HamshavathiM@users.noreply.github.com>
2022-03-15 14:44:07 -0700

    Create DictionaryExample.cs

---
+++ C#-Code-Samples/DictionaryExample.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Net.Http;
+using System.Text;
+// NOTE: Install the Newtonsoft.Json NuGet package.
+using Newtonsoft.Json;
+
+namespace TranslatorTextQuickStart
+{
+    class Program
+    {
+        private const string region_var = "TRANSLATOR_SERVICE_REGION";
+        private static readonly string region = Environment.GetEnvironmentVariable(region_var);
+
+        private const string key_var = "TRANSLATOR_TEXT_SUBSCRIPTION_KEY";
+        private static readonly string subscriptionKey = Environment.GetEnvironmentVariable(key_var);
+
+        private const string endpoint_var = "TRANSLATOR_TEXT_ENDPOINT";
+        private static readonly string endpoint = Environment.GetEnvironmentVariable(endpoint_var);
+
+        static Program()
+        {
+            if (null == region)
+            {
+                throw new Exception("Please set/export the environment variable: " + region_var);
+            }
+            if (null == subscriptionKey)
+            {
+                throw new Exception("Please set/export the environment variable: " + key_var);
+            }
+            if (null == endpoint)
+            {
+                throw new Exception("Please set/export the environment variable: " + endpoint_var);
+            }
+        }
+
+        static string route = "/dictionary/examples?api-version=3.0";
+        // Translate from English to French.
+        static string params_ = "&from=en&to=fr";
+
+        static string uri = endpoint + route + params_;
+
+        static string text = "great";
+        static string translation = "formidable";
+
+        async static void Examples()
+        {
+            System.Object[] body = new System.Object[] { new { Text = text, Translation = translation } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", region);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
+
+                Console.OutputEncoding = UnicodeEncoding.UTF8;
+                Console.WriteLine(result);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Examples();
+            Console.ReadKey();
+        }
+    }
+}
