fbad517b92f6cd4dfd9acc1f901c170517adef8c
HamshavathiM <101436046+HamshavathiM@users.noreply.github.com>
2022-03-15 14:49:28 -0700

    Create Transliterate.cs

---
+++ C#-Code-Samples/Transliterate.cs
@@ -0,0 +1,90 @@
+// This sample requires C# 7.1 or later for async/await.
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+// Install Newtonsoft.Json with NuGet
+using Newtonsoft.Json;
+
+namespace TransliterateTextSample
+{
+    /// <summary>
+    /// The C# classes that represents the JSON returned by the Translator Text API.
+    /// </summary>
+    public class TransliterationResult
+    {
+        public string Text { get; set; }
+        public string Script { get; set; }
+    }
+
+    class Program
+    {
+        private const string region_var = "TRANSLATOR_SERVICE_REGION";
+        private static readonly string region = Environment.GetEnvironmentVariable(region_var);
+
+        private const string key_var = "TRANSLATOR_TEXT_SUBSCRIPTION_KEY";
+        private static readonly string subscriptionKey = Environment.GetEnvironmentVariable(key_var);
+
+        private const string endpoint_var = "TRANSLATOR_TEXT_ENDPOINT";
+        private static readonly string endpoint = Environment.GetEnvironmentVariable(endpoint_var);
+
+        static Program()
+        {
+            if (null == region)
+            {
+                throw new Exception("Please set/export the environment variable: " + region_var);
+            }
+            if (null == subscriptionKey)
+            {
+                throw new Exception("Please set/export the environment variable: " + key_var);
+            }
+            if (null == endpoint)
+            {
+                throw new Exception("Please set/export the environment variable: " + endpoint_var);
+            }
+        }
+
+        // Async call to the Translator Text API
+        static public async Task TransliterateTextRequest(string subscriptionKey, string endpoint, string route, string inputText)
+        {
+            object[] body = new object[] { new { Text = inputText } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request.
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(endpoint + route);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", region);
+
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string result = await response.Content.ReadAsStringAsync();
+                TransliterationResult[] deserializedOutput = JsonConvert.DeserializeObject<TransliterationResult[]>(result);
+                // Iterate over the deserialized results.
+                foreach (TransliterationResult o in deserializedOutput)
+                {
+                    Console.WriteLine("Transliterated to {0} script: {1}", o.Script, o.Text);
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            // This is our main function.
+            // Output languages are defined in the route.
+            // For a complete list of options, see API reference.
+            // https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-transliterate
+            string route = "/transliterate?api-version=3.0&language=ja&fromScript=jpan&toScript=latn";
+            string textToTransliterate = @"こんにちは";
+            await TransliterateTextRequest(subscriptionKey, endpoint, route, textToTransliterate);
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
