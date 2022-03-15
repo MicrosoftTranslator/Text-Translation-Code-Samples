c1d2f7dd9c6eee295552067e11e9782250b39ac3
HamshavathiM <101436046+HamshavathiM@users.noreply.github.com>
2022-03-15 14:45:27 -0700

    Create DictionaryLookup.cs

---
+++ C#-Code-Samples/DictionaryLookup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace AltTranslation
+{
+    class Program
+    {
+        private const string region_var = "TRANSLATOR_SERVICE_REGION";
+        private static readonly string region = Environment.GetEnvironmentVariable(region_var);
+
+        private const string key_var = "TRANSLATOR_TEXT_SUBSCRIPTION_KEY";
+        private static readonly string subscriptionKey = Environment.GetEnvironmentVariable(key_var);
+
+        private const string endpoint_var = "TRANSLATOR_TEXT_ENDPOINT";
+        private static readonly string endpoint = Environment.GetEnvironmentVariable(endpoint_var);
+
+        static Program()
+        {
+            if (null == region)
+            {
+                throw new Exception("Please set/export the environment variable: " + region_var);
+            }
+            if (null == subscriptionKey)
+            {
+                throw new Exception("Please set/export the environment variable: " + key_var);
+            }
+            if (null == endpoint)
+            {
+                throw new Exception("Please set/export the environment variable: " + endpoint_var);
+            }
+        }
+
+        static void AltTranslation()
+        {
+            string route = "/dictionary/lookup?api-version=3.0&from=en&to=es";
+
+            System.Object[] body = new System.Object[] { new { Text = @"Elephants" } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Set the method to POST
+                request.Method = HttpMethod.Post;
+                // Construct the full URI
+                request.RequestUri = new Uri(endpoint + route);
+                // Add the serialized JSON object to your request
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                // Add the authorization header
+                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", region);
+                
+                // Send request, get response
+                var response = client.SendAsync(request).Result;
+                var jsonResponse = response.Content.ReadAsStringAsync().Result;
+                // Print the response
+                Console.WriteLine(jsonResponse);
+                Console.WriteLine("Press any key to continue.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            AltTranslation();
+            Console.ReadKey();
+        }
+    }
+}
