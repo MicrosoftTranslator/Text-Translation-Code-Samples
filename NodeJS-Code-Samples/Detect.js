62f711fc86dac31dae40cae93ac29d68709096e7
HamshavathiM <101436046+HamshavathiM@users.noreply.github.com>
2022-03-16 13:10:00 -0700

    Create Detect.js

---
+++ NodeJS-Code-Samples/Detect.js
@@ -0,0 +1,55 @@
+/* This simple app uses the '/detect' resource to identify the language of
+the provided text or texts. */
+
+/* This template relies on the request module, a simplified and user friendly
+way to make HTTP requests. */
+const request = require('request');
+const uuidv4 = require('uuid/v4');
+
+var key_var = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY';
+if (!process.env[key_var]) {
+    throw new Error('Please set/export the following environment variable: ' + key_var);
+}
+var subscriptionKey = process.env[key_var];
+
+var region_var = 'TRANSLATOR_RESOURCE_REGION';
+if (!process.env[region_var]) {
+    throw new Error('Please set/export the following environment variable: ' + region_var);
+}
+var region = process.env[region_var];
+
+var endpoint_var = 'TRANSLATOR_TEXT_ENDPOINT';
+if (!process.env[endpoint_var]) {
+    throw new Error('Please set/export the following environment variable: ' + endpoint_var);
+}
+var endpoint = process.env[endpoint_var];
+
+/* If you encounter any issues with the base_url or path, make sure that you are
+using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-detect */
+function detectLanguage(){
+    let options = {
+        method: 'POST',
+        baseUrl: endpoint,
+        url: 'detect',
+        qs: {
+          'api-version': '3.0',
+        },
+        headers: {
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+          'Ocp-Apim-Subscription-Region': region,
+          'Content-type': 'application/json',
+          'X-ClientTraceId': uuidv4().toString()
+        },
+        body: [{
+              'text': 'Salve, mondo!'
+        }],
+        json: true,
+    };
+
+    request(options, function(err, res, body){
+        console.log(JSON.stringify(body, null, 4));
+    });
+};
+
+// Call the function to identify the language of the provided text.
+detectLanguage();
