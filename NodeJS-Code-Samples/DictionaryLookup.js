8038215bdf21305c4ce20a60ead01e2fdec05c2c
HamshavathiM <101436046+HamshavathiM@users.noreply.github.com>
2022-03-16 13:12:28 -0700

    Create DictionaryLookup.js

---
+++ NodeJS-Code-Samples/DictionaryLookup.js
@@ -0,0 +1,57 @@
+/* This simple app uses the '/dictionary/lookup' resource to return alternative
+translations for a word and a small number of idiomatic phrases. */
+
+/* This template relies on the request module, a simplified and user friendly
+way to make HTTP requests. */
+const request = require('request');
+const uuidv4 = require('uuid/v4');
+
+var key_var = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY';
+if (!process.env[key_var]) {
+    throw new Error('Please set/export the following environment variable: ' + key_var);
+}
+var subscriptionKey = process.env[key_var];
+
+var region_var = 'TRANSLATOR_RESOURCE_REGION';
+if (!process.env[region_var]) {
+    throw new Error('Please set/export the following environment variable: ' + region_var);
+}
+var region = process.env[region_var];
+
+var endpoint_var = 'TRANSLATOR_TEXT_ENDPOINT';
+if (!process.env[endpoint_var]) {
+    throw new Error('Please set/export the following environment variable: ' + endpoint_var);
+}
+var endpoint = process.env[endpoint_var];
+
+/* If you encounter any issues with the base_url or path, make sure that you are
+using the latest endpoint: https://docs.microsoft.com/en-us/azure/cognitive-services/translator/reference/v3-0-dictionary-lookup */
+function dictionaryLookup(){
+    let options = {
+        method: 'POST',
+        baseUrl: endpoint,
+        url: 'dictionary/lookup',
+        qs: {
+          'api-version': '3.0',
+          'from': 'en',
+          'to': 'es'
+        },
+        headers: {
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+          'Ocp-Apim-Subscription-Region': region,
+          'Content-type': 'application/json',
+          'X-ClientTraceId': uuidv4().toString()
+        },
+        body: [{
+              'text': 'Elephants'
+        }],
+        json: true,
+    };
+
+    request(options, function(err, res, body){
+        console.log(JSON.stringify(body, null, 4));
+    });
+};
+
+// Call the function to return alternate translations.
+dictionaryLookup();
